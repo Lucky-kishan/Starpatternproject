428698349d98e57d7551b1adbd18cbdf06af8b19
Lucky-kishan <luckykishanrealme@gmail.com>
2023-08-01 17:16:45 +0530

    star pattern

--- project9.c
+++ project9.c
@@ -2,12 +2,7 @@
 
     int main(){
 int i, j;
-printf("Please enter the number of row:\n");
-//scanf("%d", &i);
-printf ("Please enter the number of colum:\n");
-//scanf("%d", &j);
     printf ("Here's the magic begain\n");
-
 for (i=1; i<=5; i++){
     for (j=i; j<=5; j++){
         printf("*");
